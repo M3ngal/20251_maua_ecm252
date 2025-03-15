2c862b88b48188dbcd5e93cefa7da9d02a89a08d
FERNANDO MINHARRO ALVES GIMENEZ <22.00833-0@maua.br>
2025-03-15 10:55:06 -0300

    learn(04-react): encerra a apostila

--- componentes-props/src/App.jsx
+++ componentes-props/src/App.jsx
@@ -1,41 +1,74 @@
 import Pedido from "./Pedido"
+import Cartao from "./Cartao"
+import Feedback from "./Feedback"
 
 const App = () => {
+  const textoOK = 'Já chegou'
+  const textoNOK = 'Não chegou ainda'
+  const funcaoOK = () => alert('Agradecemos a confirmação')
+  const funcaoNOK = () => alert('Verificaremos o ocorrido')
+  const componenteFeedback = (
+    <Feedback
+      textoOK={textoOK}
+      textoNOK={textoNOK}
+      funcaoOK={funcaoOK}
+      funcaoNOK={funcaoNOK}
+    />
+  )
   return (
     <div className="container border mt-2">
       <div className="row">
-        <div className="col-sm-12 col-lg-6 col-xxl-3">
-          <Pedido 
-          data="22/02/2024"
-          titulo="SSD"
-          descricao="Um SSD de 512Gb"
-          icone="hdd"/>
+        <div className="col-sm-12 col-lg-6 col-xxl-4">
+
+          <Cartao
+            cabecalho={new Date().toDateString()}>
+            <Pedido 
+            titulo="SSD"
+            descricao="Um SSD de 512Gb"
+            icone="hdd"/>
+            {componenteFeedback}
+          </Cartao>
+
         </div>
-        <div className="col-sm-12 col-lg-6 col-xxl-3">
-          <Pedido 
-          data="21/03/2023"
-          titulo="Livro"
-          descricao="Harry Potter"
-          icone="book"/>
+        <div className="col-sm-12 col-lg-6 col-xxl-4">
+
+          <Cartao
+          cabecalho={new Date().toDateString()}>
+           <Pedido 
+            titulo="Livro"
+            descricao="Harry Potter"
+            icone="book"/>
+            {componenteFeedback}
+          </Cartao>
+          
         </div>
-        <div className="col-sm-12 col-lg-6 col-xxl-3">
-          <Pedido 
-          data="23/01/2022"
-          titulo="Hipopótamo"
-          descricao="Sim, um hipo!"
-          icone="hippo"/>
+        <div className="col-sm-12 col-lg-6 col-xxl-4">
+
+          <Cartao
+          cabecalho={new Date().toDateString()}>
+            <Pedido 
+            titulo="Hipopótamo"
+            descricao="Sim, um hipo!"
+            icone="hippo"/>
+            {componenteFeedback}
+          </Cartao>
+
         </div>
-        <div className="col-sm-12 col-lg-6 col-xxl-3">
-          <Pedido 
-          data="29/06/2025"
-          titulo="Boneco de neve"
-          descricao="Um boneco de neve que balança sozinho"
-          icone="snowman fa-shake"/>
+        <div className="col-sm-12 col-lg-6 col-xxl-4">
+
+          <Cartao
+          cabecalho={new Date().toDateString()}>
+            <Pedido 
+            titulo="Boneco de neve"
+            descricao="Um boneco de neve que balança sozinho"
+            icone="snowman fa-shake"/>
+            {componenteFeedback}
+          </Cartao>
+
         </div>
       </div>
     </div>
   )
 }
-
 
 export default App
--- componentes-props/src/Pedido.jsx
+++ componentes-props/src/Pedido.jsx
@@ -1,7 +1,6 @@
 const Pedido = (props) => {
-    return (<div className="card">
-        <div className="card-header text-muted">{props.data}</div>
-        <div className="card-body d-flex">
+    return (
+        <div className="d-flex">
             <div className="d-flex justify-content-center flex-column">
                 <i className={`fa-solid fa-${props.icone} fa-2x`}></i>
             </div>
@@ -9,7 +8,6 @@ const Pedido = (props) => {
                 <h4 className="text-center">{props.titulo}</h4>
                 <p className="text-center">{props.descricao}</p>
             </div>
-        </div>
-    </div>)
+        </div>)
 }
 export default Pedido
