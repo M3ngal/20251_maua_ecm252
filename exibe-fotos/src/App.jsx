5608acbce3675f58b2f8d81b600347237593e7ca
FERNANDO MINHARRO ALVES GIMENEZ <22.00833-0@maua.br>
2025-05-10 12:57:53 -0300

    learn(07-react): encerra a apostila
    -retomar fazendo o back end entre front e API Pexels a fim de isolar chave

--- exibe-fotos/src/App.jsx
+++ exibe-fotos/src/App.jsx
@@ -1,15 +1,61 @@
+// import { createClient } from 'pexels'
+import pexelsClient from './utils/pexelsClient'
+import env from 'react-dotenv'
 import React from "react"
 import Busca from "./components/Busca"
+import ListaImagens from './components/ListaImagens'
+import PexelsLogo from './components/PexelsLogo'
 
-const App = () => {
-  return (
-    <div className="grid border-1 border-200 justify-content-center w-9 m-auto">
-      <div className="col-12">
-        <h4>Exibir uma lista de...</h4>
+export default class App extends React.Component {
+
+  // pexelsClient = null
+
+  state = {
+    photos: []
+  }
+
+  onBuscaRealizada = (termo) => {
+    pexelsClient.get('/search', {
+      params: {
+        query: termo,
+        per_page: 1
+      }
+    })
+      .then(result => this.setState({ photos: result.data.photos }))
+  }
+
+  // onBuscaRealizada = (termo) => {
+  //   this.pexelsClient.photos.search({
+  //     query: termo
+  //   })
+  //   .then(result => this.setState({photos: result.photos}))
+  // }
+
+  // componentDidMount(){
+  //   console.log(env.PEXELS_KEY)
+  //   this.pexelsClient = createClient(env.PEXELS_KEY)
+  // }
+
+  render() {
+    return (
+      <div className="grid border-1 border-200 justify-content-center w-9 m-auto">
+        <div className="col-12">
+          <PexelsLogo />
+        </div>
+        <div className="col-12">
+          <h4>Exibir uma lista de...</h4>
+        </div>
+        <div className="col-12">
+          <Busca onBuscaRealizada={this.onBuscaRealizada} />
+        </div>
+        <div className="col-12">
+          <div className='grid'>
+            <ListaImagens
+              imgStyle={'md:col-6 lg:col-4 xl:col-3'}
+              photos={this.state.photos} />
+          </div>
+        </div>
       </div>
-    <Busca />
-  </div>
-  )
+    )
+  }
 }
-
-export default App
