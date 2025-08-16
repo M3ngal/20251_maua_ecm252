6e4e41db54cf26dbfc00da9c7188cf975a9b3cbd
FERNANDO MINHARRO ALVES GIMENEZ <22.00833-0@maua.br>
2025-08-16 12:48:25 -0300

    feat: dart project

---
+++ projetos_dart/introducao/intro.dart
@@ -0,0 +1,21 @@
+void main(){
+String nome = "João"; //aspas duplas
+String sobrenome = 'Silva'; //aspas simples
+String endereco = '''
+Rua B,
+número 1234, Vila J'''; //aspas simples triplas
+bool deMaior = false;
+int idade = 17;
+num peso = 80.5;
+double altura = 1.82;
+//string raw
+String comoPularLinha = r"Pule uma linha com \n";
+print (nome);
+print(sobrenome);
+print(endereco);
+print(deMaior);
+print(idade);
+print(peso);
+print(altura);
+print(comoPularLinha);
+}
